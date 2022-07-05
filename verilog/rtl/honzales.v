67469a76db42e51422d47640801dd824fa52b0dd
Gon Solo <gonsolo@gmail.com>
2022-07-05 10:58:45 +0200

    Test input.
    Set IO pin 33 to one and loop it through the chip. If it is set to 0
    nothing is counted and the testbench fails.

--- verilog/rtl/honzales.v
+++ verilog/rtl/honzales.v
@@ -1,6 +1,7 @@
 module Honzales(
   input         clock,
   input         reset,
+  input         io_input,
   output [31:0] io_output
 );
 `ifdef RANDOMIZE_REG_INIT
@@ -8,7 +9,8 @@ module Honzales(
 `endif // RANDOMIZE_REG_INIT
   reg [7:0] counterValue; // @[Counter.scala 62:40]
   wire [7:0] _wrap_value_T_1 = counterValue + 8'h1; // @[Counter.scala 78:24]
-  assign io_output = {{24'd0}, counterValue}; // @[Honzales.scala 15:13]
+  wire [7:0] _io_output_T = io_input ? counterValue : 8'h0; // @[Honzales.scala 15:19]
+  assign io_output = {{24'd0}, _io_output_T}; // @[Honzales.scala 15:13]
   always @(posedge clock) begin
     if (reset) begin // @[Counter.scala 62:40]
       counterValue <= 8'h0; // @[Counter.scala 62:40]
@@ -65,17 +67,21 @@ endmodule
 module Testbench(
   input         clock,
   input         reset,
+  input         io_input,
   output [31:0] io_output
 );
   wire  honzales_clock; // @[Testbench.scala 14:24]
   wire  honzales_reset; // @[Testbench.scala 14:24]
+  wire  honzales_io_input; // @[Testbench.scala 14:24]
   wire [31:0] honzales_io_output; // @[Testbench.scala 14:24]
   Honzales honzales ( // @[Testbench.scala 14:24]
     .clock(honzales_clock),
     .reset(honzales_reset),
+    .io_input(honzales_io_input),
     .io_output(honzales_io_output)
   );
-  assign io_output = honzales_io_output; // @[Testbench.scala 15:15]
+  assign io_output = honzales_io_output; // @[Testbench.scala 16:22]
   assign honzales_clock = clock;
   assign honzales_reset = reset;
+  assign honzales_io_input = io_input; // @[Testbench.scala 15:21]
 endmodule
